f229ab7024379a6e3fceafe91cc1355cc47ae5c0
sergeyodessa81 <sergey.gulko99@gmail.com>
2025-09-17 03:19:16 -0700

    Add marketing About page

---
+++ app/(marketing)/about/page.tsx
@@ -0,0 +1,8 @@
+export default function Page() {
+  return (
+    <div className="prose">
+      <h1>About</h1>
+      <p>\u041a\u043e\u0440\u043e\u0442\u043a\u043e \u043e \u0442\u0435\u0431\u0435 + \u043c\u0438\u0441\u0441\u0438\u044f.</p>
+    </div>
+  );
+}
